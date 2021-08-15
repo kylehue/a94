a41419e030a6e3b8519452a1d0d232fbabaea7a2
kylehue <apeuda69@gmail.com>
2021-08-15 20:11:42 +0800

    Now listening to client messages and room joins

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,6 @@
+//Load modules
+const Room = require("./room.js");
+
 //Setup server
 const express = require("express");
 const socket = require("socket.io");
@@ -11,6 +14,80 @@ const server = app.listen(port, () => {
 const io = socket(server);
 app.use(express.static(__dirname + "/../client"));
 
+//Variables
+let _lengthAuto = 0;
+
+//Things
+const codes = [];
+const rooms = [];
+
+//Functions
+function getCode() {
+	let length = 6;
+	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let code = "";
+
+	function generateCode() {
+		code = "";
+		let len = _lengthAuto || length;
+		for (var i = 0; i < len; i++) {
+			code += chars[Math.floor(Math.random() * chars.length)];
+		}
+	}
+
+	generateCode();
+
+	let start = Date.now();
+	while (codes.includes(code)) {
+		if (Date.now() - start > 20) {
+			_lengthAuto = !_lengthAuto ? length + 1 : _lengthAuto + 1;
+		}
+
+		generateCode();
+	}
+
+	codes.push(code);
+	return code;
+}
+
 io.on("connection", socket => {
 	console.log(`${socket.id} has connected.`);
+
+	socket.on("userJoin", data => {
+		data = data || {};
+
+		if (!data.code) data.code = getCode();
+
+		let room = rooms.find(rm => rm.code === data.code);
+
+		//Create new room if the code is unique
+		if (!room) {
+			room = new Room(data.code);
+			rooms.push(room);
+
+			socket.emit("autoCode", data.code);
+		}
+
+		socket.join(data.code);
+
+		let user = {
+			id: socket.id,
+			name: data.name
+		};
+
+		room.addUser(user);
+
+		console.log(room);
+	});
+
+	socket.on("sendMessage", (roomCode, msgData) => {
+		let room = rooms.find(rm => rm.code === roomCode);
+
+		console.log(roomCode, msgData);
+
+		if (room) {
+			room.addMessage(msgData);
+			io.in(room.code).emit("newMessage", msgData);
+		}
+	});
 });
